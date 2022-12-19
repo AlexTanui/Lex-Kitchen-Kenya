ab7088d1751c64c257fbde09ac613d34666b662b
AlexTanui <tanuialex417@gmail.com>
2022-12-19 20:36:11 +0300

    commented footer

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,12 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
-const listofmeals= document.getElementById('listofmeals')
-const btnKenya =document.getElementById('kenyameals')
-const kenyaMealapp= document.getElementById('btnKenya')
+const listofmeals = document.getElementById('listofmeals')
+const btnKenya = document.getElementById('kenyameals')
+const kenyaMealapp = document.getElementById('btnKenya')
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
-const hidesection= document.getElementById("hide-meals").style.display = "none";
-const form =document.getElementById('addIngridients')
+const hidesection = document.getElementById("hide-meals").style.display = "none";
+const form = document.getElementById('addIngridients')
 
 // for scrolling meals to right and left
 const productContainers = [...document.querySelectorAll('.product')];
@@ -20,7 +20,7 @@ productContainers.forEach((item, i) => {
 
     nxtBtn[i].addEventListener('click', () => {
         item.scrollLeft += containerWidth;
-        
+
     })
 
     preBtn[i].addEventListener('click', () => {
@@ -34,26 +34,26 @@ searchBtn.addEventListener('click', getMealList);
 kenyaMealapp.addEventListener('click', foodKenya)
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
-mealDetailsContent.parentElement.classList.remove('showRecipe');
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
 
 // funtion to display section and hide log in
-function showItems(){
-document.getElementById("hide-meals").style.display = "block";
-document.getElementById("sign-in").style.display = "none";
+function showItems() {
+    document.getElementById("hide-meals").style.display = "block";
+    document.getElementById("sign-in").style.display = "none";
 }
 
 // function to get meal list that matches with the ingredients
-function getMealList(){
+function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
-                html += `
+        .then(response => response.json())
+        .then(data => {
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
                     <div class = "meal-item" data-id = "${meal.idMeal}">
                         <div class = "meal-img">
                             <img src = "${meal.strMealThumb}" alt = "food">
@@ -64,31 +64,31 @@ function getMealList(){
                         </div>
                     </div>
                 `;
-            });
-            mealList.classList.remove('notFound');
-        } else{
-            html = "Sorry, we didn't find any meal!";
-            mealList.classList.add('notFound');
-        }
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find any meal!";
+                mealList.classList.add('notFound');
+            }
 
-        mealList.innerHTML = html;
-    });
+            mealList.innerHTML = html;
+        });
 }
 
 
 // get recipe of the meal
-function getMealRecipe(e){
+function getMealRecipe(e) {
     e.preventDefault();
-    if(e.target.classList.contains('recipe-btn')){
+    if (e.target.classList.contains('recipe-btn')) {
         let mealItem = e.target.parentElement.parentElement;
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-        .then(response => response.json())
-        .then(data => mealRecipeModal(data.meals));
+            .then(response => response.json())
+            .then(data => mealRecipeModal(data.meals));
     }
 }
 
 // create a modal for international meals
-function mealRecipeModal(meal){
+function mealRecipeModal(meal) {
     console.log(meal);
     meal = meal[0];
     let html = `
@@ -111,18 +111,19 @@ function mealRecipeModal(meal){
 
 // funtion to create kenya meals
 fetch(`https://api.npoint.io/2eea172ce6ce1997759e/meals/`).then((response) => response.json())
-.then(json=>{json.map(data=>
-   
-    {console.log(data)
-        kenyaMealapp.append(foodKenya(data))
-    
-    })})
- 
-    
+    .then(json => {
+        json.map(data => {
+            console.log(data)
+            kenyaMealapp.append(foodKenya(data))
 
-function foodKenya ({strFood,strMealThumb}) {
-    let container =document.createElement("container")
-   container.innerHTML =`
+        })
+    })
+
+
+
+function foodKenya({ strFood, strMealThumb }) {
+    let container = document.createElement("container")
+    container.innerHTML = `
                  <div id="space" class = "meal-item">
                 <div class = "meal-img">
                       <img src = "${strMealThumb}" alt = "Food">
@@ -140,46 +141,46 @@ function foodKenya ({strFood,strMealThumb}) {
     return container
 }
 // post new meal to get ingrideients
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     // prevent auto submit
     e.preventDefault()
 
     let name = document.getElementById('strFood').value
-    let image= document.getElementById('strMealThumb').value
+    let image = document.getElementById('strMealThumb').value
 
-    fetch("https://api.npoint.io/2eea172ce6ce1997759e/meals/",{
+    fetch("https://api.npoint.io/2eea172ce6ce1997759e/meals/", {
         method: 'POST',
-        body:JSON.stringify({
-            strMeal:strFood,
-            strMealThumb:strMealThumb
+        body: JSON.stringify({
+            strMeal: strFood,
+            strMealThumb: strMealThumb
         }),
-        headers:{
-            "content-Type":"applictaion/json; charset=UTF-8"
+        headers: {
+            "content-Type": "applictaion/json; charset=UTF-8"
         }
     })
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        })
 })
 // funtion to display section and hide log in
-function showItems(){
-document.getElementById("hide-meals").style.display = "block";
-document.getElementById("sign-in").style.display = "none";
+function showItems() {
+    document.getElementById("hide-meals").style.display = "block";
+    document.getElementById("sign-in").style.display = "none";
 }
 
 // function to get meal list that matches with the ingredients
-function getMealList(){
+function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
-                html += `
+        .then(response => response.json())
+        .then(data => {
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
                     <div class = "meal-item" data-id = "${meal.idMeal}">
                         <div class = "meal-img">
                             <img src = "${meal.strMealThumb}" alt = "food">
@@ -190,14 +191,13 @@ function getMealList(){
                         </div>
                     </div>
                 `;
-            });
-            mealList.classList.remove('notFound');
-        } else{
-            html = "Sorry, we didn't find any meal!";
-            mealList.classList.add('notFound');
-        }
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find any meal!";
+                mealList.classList.add('notFound');
+            }
 
-        mealList.innerHTML = html;
-    });
+            mealList.innerHTML = html;
+        });
 }
-     
